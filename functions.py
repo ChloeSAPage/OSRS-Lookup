f2cf0c969d4d233a16bcf7ce091d4415115db908
ChloeSAPage <chloe.s.a.page@Gmail.com>
2024-03-10 17:28:54 +0000

    added functions

--- functions.py
+++ functions.py
@@ -1,1 +1,38 @@
-# function to obtain name and value then sort them. 
+import requests
+
+def get_data():
+    player = input("Enter a Player Name: ")
+    # player = "eolhx"
+    URL = f"https://secure.runescape.com/m=hiscore_oldschool/index_lite.json?player={player}"
+    response = requests.get(URL)
+    data = response.json()
+    
+    print("Data Retrieved Successfully")
+    return data
+
+
+def put_data_in_dic(data):
+    user_choice = input("Activities or Skills? ").lower()
+    chosen_data = data[user_choice]
+    cleaned_data = {}
+
+    for data in chosen_data:
+        name = data["name"]
+
+        if user_choice == "skills":
+            skill_level = data["level"]
+            cleaned_data[name] = skill_level
+
+        elif user_choice == "activities":
+            score = data["score"]
+            if score > 0:
+                cleaned_data[name] = score
+    
+    return cleaned_data, user_choice
+
+
+def write_to_file(user_choice, sorted_data):
+    with open(user_choice, "w") as file:
+        for skill_name, skill_level in sorted_data:
+            file.write(f"{skill_name}: {skill_level}\n")
+    print("Written to file successfully")
--- main.py
+++ main.py
@@ -1,41 +1,12 @@
-import requests
+from functions import put_data_in_dic, write_to_file, get_data
 
-# player =  input("Enter a Player Name: ")
-player = "player"
-URL = f"https://secure.runescape.com/m=hiscore_oldschool/index_lite.json?player={
-    player}"
-response = requests.get(URL)
-
-data = response.json()
-# print(data)
-
-# ask user if they want skills or activities?
-# user_choice = input("Do you want to see Skills or Activities: ").lower()
-user_choice = "skills"
-skills_cleaned = []
+data = get_data()
 
 # go through find all KCs or skills
-if user_choice == "skills":
-    skills = data["skills"]
-    print(skills)
-    for skill in skills:
-        print("\n", skill)
-        skill_name = skill["name"]
-        skill_level = skill["level"]
-        skills_cleaned.append((skill_name, str(skill_level)))
-        print(f"{skill_name}: {skill_level}")
-
-
-elif user_choice == "activities":
-    activities = data["activities"]
-    print(activities)
-
+cleaned_data, user_choice = put_data_in_dic(data)
 
 # order them by KC or rank
-skills_cleaned.sort(reverse=True, key=lambda a: int(a[1]))
-print(skills_cleaned)
+sorted_data = sorted(cleaned_data.items(),
+                     reverse=True, key=lambda a: a[1])
 
-# save to file
-with open(user_choice, "w") as file:
-    for skill_name, skill_level in skills_cleaned:
-        file.write(f"{skill_name}: {skill_level}\n")
+write_to_file(user_choice, sorted_data)
